88532ce570a10b352175b36bb63578aec2b85ae9
greendragon345 <yehonatan.hacham@gmail.com>
2022-11-14 16:29:44 +0200

    K calculation

--- java/Constants.java
+++ java/Constants.java
@@ -2,8 +2,13 @@ public final class Constants {
 
     public static final double g=0;
     public static double hieght =0;
-    private static final double H = 0;
-    public static final double K=0;
+    private static final double H = 0
+    public static double mass =0;
+    public statoc double airdensoty = 0;
+    public static double aerodynmiccoef =0.45;
+    public static double ballarea = 0;
+    public static final double K=airdensoty*aerodynmiccoef*ballarea
+    /2*g*mass;
     public static final double minspeed = Math.sqrt(H*g*2/
     Utils.todegsin(0.001)*(Utils.todegsin(0.001)-g*K));
     public static double hoodratio =0;
