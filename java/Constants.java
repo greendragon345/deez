3ee75f306436d3889e4a280d382203eeecc9f91b
greendragon345 <yehonatan.hacham@gmail.com>
2022-11-15 13:32:53 +0200

    Hoolo

--- java/Constants.java
+++ java/Constants.java
@@ -1,12 +1,12 @@
 public final class Constants {
 
-    public static final double g=0;
+    public static final double g=9.81;
     public static double hieght =0;
-    private static final double H = 0
-    public static double mass =0;
-    public statoc double airdensoty = 0;
-    public static double aerodynmiccoef =0.45;
-    public static double ballarea = 0;
+    private static final double H = 4;
+    public static double mass =0.27;
+    public statoc double airdensoty = 1.185;
+    public static double aerodynmiccoef=0.47;
+    public static double ballarea = 0.0144*3.14;
     public static final double K=airdensoty*aerodynmiccoef*ballarea
     /2*g*mass;
     public static final double minspeed = Math.sqrt(H*g*2/
@@ -14,5 +14,5 @@ public final class Constants {
     public static double hoodratio =0;
     public static double shootratio =0;
     
-    public static final double maxH = 0;
+    public static final double maxH = 4;
 }
